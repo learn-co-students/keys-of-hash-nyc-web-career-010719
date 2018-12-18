67d83de8e4c1b2ef3a3719a5cb72bb04131b6669
cannoli9917 <katie.pennachio@gmail.com>
2018-12-18 12:01:00 -0500

    Monkey patched hash class

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+
+
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    key_array = []
+    each do |key, value|
+        arguments.each do |place|
+            if value == place
+                key_array << key
+            end
+        end
+    end
+    key_array
   end
 end
+
+
+
+
